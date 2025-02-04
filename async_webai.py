5174c7c407f908e564569936620c16d171c5fc8d
MinhThien <nguyenanminhthien97@gmail.com>
2025-02-04 17:17:43 +0700

    success when there is only 1 URL used with aiohttp.ClientSession

--- async_webai.py
+++ async_webai.py
@@ -6,20 +6,23 @@ async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
-async def parse(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    # Perform your scraping tasks here
-    # For example, to find all 'a' tags:
+import concurrent.futures
+
+async def parse(html,loop):
+    # Use default executor (ThreadPoolExecutor) to run synchronous code
+    soup = await loop.run_in_executor(None, BeautifulSoup, html, 'html.parser')
     links = soup.find_all('a')
-    # Do something with the links
     return links
+
 
 async def main(url):
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, url)
-        links = await parse(html)
+        loop = asyncio.get_event_loop()
+        links = await parse(html,loop)
         # Do something with the results
         print(links)
+
 
 url = "https://www.fpds.gov/ezsearch/fpdsportal?q=SECURITIES+AND+EXCHANGE+COMMISSION&s=FPDS.GOV&templateName=1.5.3&indexName=awardfull&x=19&y=13"  # Replace with your target URL
 asyncio.run(main(url))
