76ab66453e5b5c0914d3495028ca2de94b23ba11
MinhThien <nguyenanminhthien97@gmail.com>
2025-02-07 07:49:44 +0700

    success scrape links at file aio_100.py

--- aio_100.py
+++ aio_100.py
@@ -2,13 +2,20 @@ import time
 
 import aiohttp
 import asyncio
+import random
+from bs4 import BeautifulSoup
 
+member_urls = list()
 async def fetch_url(url):
+    global member_urls
+    # time.sleep(random.randint(0,10))
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 200:
                 data = await response.text()
-                print(f"URL: {url}, Data: {data[:50]}...")  # Truncate for brevity
+                soup = BeautifulSoup(data, features="html.parser")
+                links = soup.find_all('a', title="View")
+                member_urls.extend([link.get('href') for link in links])
             else:
                 print(f"Error fetching {url}: {response.status}")
 
@@ -16,15 +23,16 @@ async def main(urls):
     tasks = [asyncio.create_task(fetch_url(url)) for url in urls]
     await asyncio.gather(*tasks)
 
-
-mylist = list()
-for i in range(0,1000):
-    mylist.append(i*30)
-
-urls = [
-    f"https://www.fpds.gov/ezsearch/fpdsportal?q=SECURITIES+AND+EXCHANGE+COMMISSION&s=FPDS.GOV&templateName=1.5.3&indexName=awardfull&x=19&y=13&start={number}"
-    for number in mylist]
-start = time.time()
-asyncio.run(main(urls))
-end = time.time()
-print(f"Total time: {end - start}")
+def scrape_view_link():
+    mylist = list()
+    for i in range(0,2):
+        mylist.append(i*30)
+    urls = [
+        f"https://www.fpds.gov/ezsearch/fpdsportal?indexName=awardfull&templateName=1.5.3&s=FPDS.GOV&q=SIGNED_DATE%3A%5B2020%2F06%2F21%2C2025%2F02%2F05%5D+OBLIGATED_AMOUNT%3A%5B1000%2C%29&x=11&y=13&start={number}"
+        for number in mylist]
+    start = time.time()
+    asyncio.run(main(urls))
+    print(member_urls)
+    end = time.time()
+    print(f"Total time: {end - start}")
+scrape_view_link()
